2851bf081084284c425dcca12cc9357589e9a87b
fuubi <fuubi@protonmail.ch>
2021-07-01 12:51:28 +0200

    feat: Add a custom logger.
    The logger's only purpose is to demonstrate how to
    extend the CSS server with a custom module.

---
+++ src/logging/DemoLoggerFactory.ts
@@ -0,0 +1,20 @@
+import type { Logger, LoggerFactory } from '@solid/community-server';
+import { CustomLogger } from './CustomLogger';
+
+/**
+ * Uses the winston library to create loggers for the given logging level.
+ * By default, it will print to the console with colorized logging levels.
+ *
+ * This creates instances of {@link WinstonLogger}.
+ */
+export class WinstonLoggerFactory implements LoggerFactory {
+  private readonly level: string;
+  public constructor(level: string) {
+    this.level = level;
+  }
+
+  public createLogger(label: string): Logger {
+    return new CustomLogger(label, this.level);
+  }
+}
+
